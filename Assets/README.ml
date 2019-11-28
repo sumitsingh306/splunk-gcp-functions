2b8d01a5ac7ec0056a6c40a140511e3a1d555f4a
pauld-splunk <pauld@splunk.com>
2019-11-28 09:19:58 +0000

    Update README.ml

--- Assets/README.ml
+++ Assets/README.ml
@@ -1,1 +1,6 @@
+# GCP Functions Library for Ingesting into Splunk
 
+**Assets Functions**
+
+This function periodically requests Assets configurations (API Call) and publishes it to a GCS Bucket. The GCS Function can then ingest this content into Splunk via HEC.
+The trigger for the Assets function would be done by a Cloud Schedule trigger to a PubSub Topic.
