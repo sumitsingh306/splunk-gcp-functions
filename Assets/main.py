41643d428d29ab9d6b85f34179ec5bfc88cb6ce8
pauld-splunk <pauld@splunk.com>
2019-12-16 09:35:33 +0000

    Update main.py

--- Assets/main.py
+++ Assets/main.py
@@ -1,4 +1,4 @@
-# Assets0.2
+# Assets0.3
 # Called from PubSub Topic
 # Create CRON schedule to send a PubSub to call the Function to refresh the asset inventory
 # Use GCS function template to read from GCS into HEC
@@ -23,5 +23,5 @@ def export_assets(project_id, dump_file_path):
     parent = client.project_path(project_id)
     output_config = asset_service_pb2.OutputConfig()
     output_config.gcs_destination.uri = dump_file_path
-    response = client.export_assets(parent, output_config)
+    response = client.export_assets(parent, output_config, content_type='RESOURCE')
        
